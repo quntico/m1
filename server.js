d3034c416fb75ea372165bab0fc40f013088a3d6
Freddy Arevalo <delavega3540@gmail.com>
2026-09-02 17:16:58 -0600

    fix(server): expand json payload limit to allow high-res base64 branding uploads via backend api

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(cors({
         }
     }
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Directories
 const projectBasePath = path.join(__dirname, 'data', 'projects', 'M1');
